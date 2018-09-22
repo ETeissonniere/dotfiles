d09348ed519ce055f9eb4b6b7488919a9d4872d0
Eliott Teissonniere <gitlab.eliott@teissonniere.org>
2018-09-22 15:32:40 +0200

    SSH config

--- update.sh
+++ update.sh
@@ -2,3 +2,6 @@ ln -sf `pwd`/vim ~/.vim
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/gitignore ~/.gitignore
+
+mkdir -p ~/.ssh
+ln -sf `pwd`/ssh_config ~/.ssh/config
