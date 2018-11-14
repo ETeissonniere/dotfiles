0a57940c8d84aa6a31fa13a117e22c4e085863f7
Eliott Teissonniere <git.eliott@teissonniere.org>
2018-11-14 21:22:47 +0100

    Install a few packages

---
+++ install.sh
@@ -0,0 +1,11 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew analytics off
+
+brew cask install firefox
+brew cask install docker
+brew cask install sublime-text
+
+brew install go
+
+./update.sh
