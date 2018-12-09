e1eefb18e54a1c3a2967f4ab47ad3a8589b591bd
Eliott Teissonniere <git.eliott@teissonniere.org>
2018-12-09 11:09:46 +0100

    Mac apps

--- install.sh
+++ install.sh
@@ -1,34 +1,10 @@
 networksetup -setdnsservers Wi-Fi 1.1.1.1
 
 xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew analytics off
-
-brew cask install google-chrome
-brew cask install docker
-brew cask install sublime-text
-brew cask install drawio
-brew cask install ledger-live
-
-brew install git
-brew install hub
-brew install go
-brew install node
-brew install yarn
-brew install coffeescript
-brew install kubectl
-brew install bash-completion
-
-brew install mas
-mas lucky Pages
-mas lucky Keynote
-mas lucky Numbers
-mas lucky Slack
-mas lucky Xcode
 
 git clone https://github.com/ETeissonniere/dotfiles ~/.dotfiles
 cd ~/.dotfiles
 
 ./update.sh
 ./mac_defaults.sh
+./mac_apps.sh
---
+++ mac_apps.sh
@@ -0,0 +1,26 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew analytics off
+
+brew cask install google-chrome
+brew cask install docker
+brew cask install sublime-text
+brew cask install drawio
+brew cask install ledger-live
+
+brew install git
+brew install hub
+brew install go
+brew install node
+brew install yarn
+brew install coffeescript
+brew install kubectl
+brew install bash-completion
+brew install dockutil
+
+#brew install mas
+#mas lucky Pages
+#mas lucky Keynote
+#mas lucky Numbers
+#mas lucky Slack
+#mas lucky Xcode
