99fe09f281228b48015d6282d8c4afa9651edb3d
Eliott Teissonniere <git.eliott@teissonniere.org>
2018-12-09 12:01:30 +0100

    Wait for tools to be installed

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 networksetup -setdnsservers Wi-Fi 1.1.1.1
 
 xcode-select --install
+read -p "[DOTFILES] Press enter when command lines tools are installed"
 
 git clone https://github.com/ETeissonniere/dotfiles ~/.dotfiles
 cd ~/.dotfiles
