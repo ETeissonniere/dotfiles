0663cf7a172f74bfd280f2ded3ca7e8ef2dfde47
Eliott Teissonniere <git.eliott@teissonniere.org>
2018-12-10 08:58:57 +0100

    We forgot the Downloads folder

--- mac_dock.sh
+++ mac_dock.sh
@@ -1,14 +1,16 @@
-dockutil --remove all
-dockutil --add /Applications/Safari.app
-dockutil --add /Applications/Google\ Chrome.app
-dockutil --add /Applications/Mail.app
-dockutil --add /Applications/Slack.app
-dockutil --add /Applications/Keynote.app
-dockutil --add /Applications/Numbers.app
-dockutil --add /Applications/Pages.app
-dockutil --add /Applications/App\ Store.app
-dockutil --add /Applications/draw.io.app
-dockutil --add /Applications/Sublime\ Text.app
-dockutil --add /Applications/Utilities/Terminal.app
-dockutil --add /Applications/Docker.app
-dockutil --add /Applications/Ledger\ Live.app
+dockutil --no-restart --remove all
+dockutil --no-restart --add /Applications/Safari.app
+dockutil --no-restart --add /Applications/Google\ Chrome.app
+dockutil --no-restart --add /Applications/Mail.app
+dockutil --no-restart --add /Applications/Slack.app
+dockutil --no-restart --add /Applications/Keynote.app
+dockutil --no-restart --add /Applications/Numbers.app
+dockutil --no-restart --add /Applications/Pages.app
+dockutil --no-restart --add /Applications/App\ Store.app
+dockutil --no-restart --add /Applications/draw.io.app
+dockutil --no-restart --add /Applications/Sublime\ Text.app
+dockutil --no-restart --add /Applications/Utilities/Terminal.app
+dockutil --no-restart --add /Applications/Docker.app
+dockutil --no-restart --add /Applications/Ledger\ Live.app
+dockutil --no-restart --add ~/Downloads
+killall Dock # Restart
