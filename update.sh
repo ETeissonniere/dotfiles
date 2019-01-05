59c0a6991a56265bca5688e18c3186a33702ef0b
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-05 11:01:58 +0100

    No ssh config (handled somewhere else)

--- update.sh
+++ update.sh
@@ -3,6 +3,3 @@ ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/gitignore ~/.gitignore
 ln -sf `pwd`/zshrc ~/.zshrc
-
-mkdir -p ~/.ssh
-ln -sf `pwd`/ssh_config ~/.ssh/config
