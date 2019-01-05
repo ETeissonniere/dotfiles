0b16ef9bbd6ff01dc49d7812ca5a5e6b9269f6c1
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-05 11:26:55 +0100

    Rework dock

--- mac_dock.sh
+++ mac_dock.sh
@@ -6,10 +6,12 @@ dockutil --no-restart --add /Applications/Slack.app
 dockutil --no-restart --add /Applications/Keynote.app
 dockutil --no-restart --add /Applications/Numbers.app
 dockutil --no-restart --add /Applications/Pages.app
+dockutil --no-restart --add /Applications/Notes.app
+dockutil --no-restart --add /Applications/Reminders.app
 dockutil --no-restart --add /Applications/App\ Store.app
 dockutil --no-restart --add /Applications/draw.io.app
-dockutil --no-restart --add /Applications/Sublime\ Text.app
-dockutil --no-restart --add /Applications/Utilities/Terminal.app
+dockutil --no-restart --add /Applications/Android\ Studio.app
+dockutil --no-restart --add /Applications/iTerm.app
 dockutil --no-restart --add /Applications/Docker.app
 dockutil --no-restart --add /Applications/Ledger\ Live.app
 dockutil --no-restart --add ~/Downloads
