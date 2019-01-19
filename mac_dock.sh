3fb6468904573bf72f3747fc802e7c501100bd68
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-19 22:02:00 +0100

    Xcode in dock

--- mac_dock.sh
+++ mac_dock.sh
@@ -11,6 +11,7 @@ dockutil --no-restart --add /Applications/Reminders.app
 dockutil --no-restart --add /Applications/App\ Store.app
 dockutil --no-restart --add /Applications/draw.io.app
 dockutil --no-restart --add /Applications/Android\ Studio.app
+dockutil --no-restart --add /Applications/Xcode.app
 dockutil --no-restart --add /Applications/iTerm.app
 dockutil --no-restart --add /Applications/Docker.app
 dockutil --no-restart --add /Applications/Ledger\ Live.app
