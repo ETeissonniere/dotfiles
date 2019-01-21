1d9334cfd1b04271ca79c7b814cecae2c5fd26f2
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-21 09:25:38 +0100

    Change config setup script

---
+++ configs/install.sh
@@ -0,0 +1,4 @@
+ln -sf `pwd`/vimrc ~/.vimrc
+ln -sf `pwd`/gitconfig ~/.gitconfig
+ln -sf `pwd`/gitignore ~/.gitignore
+ln -sf `pwd`/zshrc ~/.zshrc
--- update.sh
+++
@@ -1,4 +0,0 @@
-ln -sf `pwd`/configs/vimrc ~/.vimrc
-ln -sf `pwd`/configs/gitconfig ~/.gitconfig
-ln -sf `pwd`/configs/gitignore ~/.gitignore
-ln -sf `pwd`/configs/zshrc ~/.zshrc
