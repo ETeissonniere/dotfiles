32ce1ebded85eeeeda3fabf25ccf734da6fb2f64
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-21 09:10:07 +0100

    Move mac preferences, add a few defaults

--- prefs/mac_system.sh
+++ prefs/mac_system.sh
@@ -16,6 +16,8 @@ defaults write -g com.apple.trackpad.scaling -int 4
 # Safari
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
 # TimeMachine
 echo "Disabling TimeMachine (need SUDO)"
@@ -24,6 +26,20 @@ sudo tmutil disable
 
 # Screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
+
+# Show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+# Require password immediately on sleep
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Let me change the file's extension!
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Mail
+# Copy email adresses correctly
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 
 # Kill affected apps
 for app in "Dock" \
