2ef446e9b9f1a2ba896ecf4baef70207015f0a7b
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-01-21 09:16:19 +0100

    Refactor apps installation

---
+++ apps/mac_dev.sh
@@ -0,0 +1,23 @@
+brew cask install docker
+open -a "Docker"
+
+brew cask install drawio
+brew cask install android-studio
+brew cask install iterm2
+
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+
+brew install git
+brew install hub
+
+brew install go
+
+brew install node
+brew install coffeescript
+
+brew install carthage
+brew install cocoapods
+
+brew install zsh
+brew install zplug
---
+++ apps/mac_sysadmin.sh
@@ -0,0 +1,8 @@
+brew install dockutil
+brew install mas
+
+mas lucky Pages
+mas lucky Keynote
+mas lucky Numbers
+mas lucky Slack
+mas lucky Xcode
