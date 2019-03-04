d61e5ce830fe55a8751ee929c4a5c0583c9b5da2
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-03-04 17:42:55 +0100

    Remove slack from dock

--- prefs/mac_dock.sh
+++ prefs/mac_dock.sh
@@ -3,7 +3,6 @@ dockutil --no-restart --add /Applications/Safari.app
 dockutil --no-restart --add /Applications/Brave\ Browser.app
 dockutil --no-restart --add /Applications/Station.app
 dockutil --no-restart --add /Applications/Mail.app
-dockutil --no-restart --add /Applications/Slack.app
 dockutil --no-restart --add /Applications/Keynote.app
 dockutil --no-restart --add /Applications/Numbers.app
 dockutil --no-restart --add /Applications/Pages.app
