60e52fcaa30f33b3fe6b7289f2e15a65f4393f55
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-05-15 08:15:41 +0200

    Better GPG config

--- configs/install.sh
+++ configs/install.sh
@@ -2,3 +2,7 @@ ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/gitignore ~/.gitignore
 ln -sf `pwd`/bash_profile ~/.bash_profile
+
+mkdir -p ~/.gnupg
+ln -sf `pwd`/gpg.conf ~/.gnupg/gpg.conf
+ln -sf `pwd`/gpg-agent.conf ~/.gnupg/gpg-agent.conf
