63c012e3cc99223a202228a4b51ca46721333742
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-06-03 16:34:13 +0200

    Auto update if repo already cloned

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
+FOLDER=~/.dotfiles
+URL=https://github.com/ETeissonniere/dotfiles
+
 do_cd_sh () {
     echo "[DOTFILES] Installing $1"
-    cd ~/.dotfiles/$1 && ./*.sh
+    cd $FOLDER/$1 && ./*.sh
 }
 
 xcode-select --install
 read -p "[DOTFILES] Press enter when command lines tools are installed"
 
-FOLDER=~/.dotfiles
-URL=https://github.com/ETeissonniere/dotfiles
 if [ ! -d "$FOLDER" ] ; then
     git clone "$URL" "$FOLDER"
+else
+    cd "$FOLDER"
+    git pull $URL
 fi
 
 do_cd_sh configs
