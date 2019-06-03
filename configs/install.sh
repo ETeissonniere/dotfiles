ed9631472697203cd33eb60834196e54f5b66cca
Eliott Teissonniere <git.eliott@teissonniere.org>
2019-06-03 16:06:01 +0200

    Configure zsh

--- configs/install.sh
+++ configs/install.sh
@@ -1,7 +1,7 @@
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/gitconfig ~/.gitconfig
 ln -sf `pwd`/gitignore ~/.gitignore
-ln -sf `pwd`/bash_profile ~/.bash_profile
+ln -sf `pwd`/zshrc ~/.zshrc
 
 mkdir -p ~/.gnupg
 ln -sf `pwd`/gpg.conf ~/.gnupg/gpg.conf
