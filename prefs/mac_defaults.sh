bdb01f68d3301099d73806f94054b7dfa528f0f7
Eliott Teissonniere <git.eliott@teissonniere.org>
2020-11-13 12:49:08 +0100

    fine tune settings to big sur

--- prefs/mac_defaults.sh
+++ prefs/mac_defaults.sh
@@ -14,9 +14,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-# Show battery percentage
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
 # Require password immediately on sleep
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
