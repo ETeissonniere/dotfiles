0f74eeeba195579a7c6e0c36a7f29d7d46f6b8ad
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-10-11 11:51:53 +0200

    run prefs only on macos

--- prefs.sh
+++ prefs.sh
@@ -1,10 +1,17 @@
-# Require password immediately on sleep
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+# See https://stackoverflow.com/a/27776822
+case "$(uname -s)" in
 
-# Firewall
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setblockall off
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
-sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+   Darwin)
+        # Require password immediately on sleep
+        defaults write com.apple.screensaver askForPassword -int 1
+        defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+        # Firewall
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setblockall off
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsigned on
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setallowsignedapp off
+        sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+     ;;
+
+esac
