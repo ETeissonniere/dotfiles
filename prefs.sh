ad854d2cf8e3d0c40fbd25fbaf68a2faffb1f746
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2024-10-08 11:46:07 -0700

    re-unify git config

--- prefs.sh
+++ prefs.sh
@@ -1,11 +1,13 @@
 ln -sf `pwd`/zshrc ~/.zshrc
+cp -f `pwd`/gitconfig ~/.gitconfig
+git config --global gpg.ssh.allowedSignersFile `pwd`/allowed_signers
+git config --global core.excludesfile `pwd`/gitignore
 
 # See https://stackoverflow.com/a/27776822
 case "$(uname -s)" in
 
      Darwin)
           # Configuration files
-          cp -f `pwd`/gitconfig ~/.gitconfig
           mkdir -p ~/.ssh
           ln -sf `pwd`/ssh ~/.ssh/config
 
@@ -37,15 +39,6 @@ case "$(uname -s)" in
           defaults write com.apple.dock autohide -bool YES
           defaults write com.apple.dock tilesize -integer 64
           killall Dock
-
-          # Properly configure git signing via 1pass
-          git config --global user.signingkey "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC3NGKCRfi2gh8mHF8u0tFFcv/H0BQyY8KHrO5OXsusUTnI0ll97Y238wt8wqo+hoea+Hzu67uT7VI58YlqMhxY52YhqlX1k5EAKhNRwvabnC11wR8cI6/Jo+W8b6o8Sf79IUMojUfg9Xc9VzGCias35+emkIeEO7QBkT2wDihFkjjIunqch3niaHS9tyM7Bd2uxZlxtFLkj4LqNcwRx6m6czrrICZpgMzTiq8ZABxiBGyhmjcKyj2PX4/5W6kYJGFLbhC9c5iUixRo2Rl590fQCX0+8y1/lOpjBfoDpLSbFONqMWxPxNOU6DS8XJEgf07zEa98ZH/NJyeeVCoUb1QVTSL13VVJARS54X5ygB0mLoUypp+JuP83fCh7b+g+P+cIxOnliGhU/1gXzMQXHX1Gb/plEJqLmzlFfBjyZnkGmkv0rdxPnt3ckWrIliALY8gqeJyf7/9IWYR7u+2ZCEfLpvKDGuJhE9BNmGQuosTiU0BXHcUXG10Gh3AVJK/Bovk="
-          git config --global commit.gpgsign true
-          git config --global gpg.format ssh
-          git config --global gpg.ssh.allowedSignersFile `pwd`/allowed_signers
-
-          # use our gitignore file, we are not duplicating our gitconfig for Codespaces
-          git config --global core.excludesfile `pwd`/gitignore
 
           # Firewall
           sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
