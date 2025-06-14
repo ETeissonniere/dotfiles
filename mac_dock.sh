d6aa7e2b09adf0edddd6dbbd3491949f48ce6366
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2025-06-14 16:14:55 -0700

    reorg doc

--- mac_dock.sh
+++ mac_dock.sh
@@ -2,21 +2,18 @@
 
 dockutil --no-restart --remove all
 
-dockutil --no-restart --add "/Applications/Arc.app"
+dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "~/Applications/Chrome Apps.localized/YouTube Music.app"
-dockutil --no-restart --add "/Applications/Superhuman.app"
+dockutil --no-restart --add "~/Applications/Youtube Music.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
-dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/Telegram.app"
-dockutil --no-restart --add "/Applications/WhatsApp.app"
-dockutil --no-restart --add "/Applications/Discord.app"
-dockutil --no-restart --add "/Applications/Notion Calendar.app"
-dockutil --no-restart --add "/Applications/Notion.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
+dockutil --no-restart --add "/System/Applications/Journal.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Reminders.app"
 dockutil --no-restart --add "/Applications/GhostTy.app"
+dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Cursor.app"
-dockutil --no-restart --add "/Applications/BambuStudio.app"
 
 dockutil --no-restart --add "~/Downloads"
 
