1e7bb9b21733bf49ccdf4eaab10175bf0d65e3fa
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2025-06-14 16:59:37 -0700

    no chrome in dock

--- mac_dock.sh
+++ mac_dock.sh
@@ -3,7 +3,6 @@
 dockutil --no-restart --remove all
 
 dockutil --no-restart --add "/Applications/Safari.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "~/Applications/Youtube Music.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
