67467350e83aed27d852994981e6bb0d337753c3
Eliott Teissonniere <10683430+ETeissonniere@users.noreply.github.com>
2025-11-22 19:37:32 -0800

    disable hot corner, fix #7

--- scripts/macos/defaults.sh
+++ scripts/macos/defaults.sh
@@ -63,6 +63,17 @@ if [ "${VM:-0}" != "1" ]; then
     defaults write com.apple.dock autohide -bool true
 fi
 defaults write com.apple.dock tilesize -int 64
+
+# Disable hot corners
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-modifier -int 0
+
 killall Dock >/dev/null 2>&1 || true
 
 # Finder
